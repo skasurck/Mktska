c6c44ad23a9ba757ee336fc7fa8fdd1dfbde5681
Irving Rangel Skasurck <skasurck@gmail.com>
2019-09-24 15:16:05 -0500

    cambios 24 sep

---
+++ js/efectos.js
@@ -0,0 +1,77 @@
+$(document).ready(function(){
+    //código a ejecutar cuando el DOM está listo para recibir instrucciones.
+    $('#quehacemos').waypoint(function(direction) {
+		quehacemos_bounce(this.element, direction); //when using waypoint, this refers to the waypoint object
+    },{offset:'50%'});
+
+    $('#tarjetaInicioDiseño').waypoint(function(direction) {
+		tarjetaInicioDiseño(this.element, direction); //when using waypoint, this refers to the waypoint object
+    },{offset:'50%'});
+    
+    
+	$('#tarjetaInicioMDigital').waypoint(function(direction) {
+		tarjetaInicioMDigital(this.element, direction); //when using waypoint, this refers to the waypoint object
+    },{offset:'50%'});
+    
+    $('#productosDecorativos').waypoint(function(direction) {
+		productosDecorativos_fadeInLeft(this.element, direction); //when using waypoint, this refers to the waypoint object
+    },{offset:'84%'});
+    
+    $('#tarjetaInicioPosicionamiento').waypoint(function(direction) {
+		tarjetaInicioPosicionamiento(this.element, direction);
+	},{offset:'49%'});
+
+    function quehacemos_bounce(target, direction){
+        if(direction === "down"){
+            $(target).addClass("animated bounceInDown");
+      
+            setTimeout(function(){
+            $(target).removeClass("animated bounceInDown");
+            $(target).removeClass("hidden");
+        }, 500);
+        }
+    }
+
+ function tarjetaInicioDiseño(target, direction){
+    if(direction === "down"){
+        $(target).addClass("animated fadeInLeft");
+  
+        setTimeout(function(){
+        $(target).removeClass("animated fadeInLeft");
+        $(target).removeClass("hidden");
+    }, 1000);
+    }
+}
+
+function tarjetaInicioMDigital(target, direction){
+    if(direction === "down"){
+        $(target).addClass("animated fadeInRight");
+  
+        setTimeout(function(){
+        $(target).removeClass("animated fadeInRight");
+        $(target).removeClass("hidden");
+    }, 3000);
+    }
+}
+
+function productosDecorativos_fadeInLeft(target, direction){
+    if(direction === "down"){
+        $(target).addClass("animated fadeInLeft");
+  
+        setTimeout(function(){
+        $(target).removeClass("animated fadeInLeft");
+        $(target).removeClass("hidden");
+    }, 3000);
+    }
+}
+
+function tarjetaInicioPosicionamiento(target, direction){
+    if(direction === "down"){
+        $(target).removeClass("hidden");
+        $(target).addClass("animated zoomIn");
+        setTimeout(function(){
+            $(target).removeClass("animated zoomIn");
+    }, 3000);
+    }
+}
+});
